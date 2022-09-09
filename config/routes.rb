810e406707f73e922f33dcc60fbd6098c98ccb9c
diego lopez <diegoalejandrolopezzapata@gmail.com>
2022-09-09 16:11:25 -0500

    update rails managr

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'tasks', to: 'tasks#index', as: :tasks
+  get 'tasks', to: 'tasks#index'
   post 'tasks', to: 'tasks#create'
   get 'tasks/new', to: 'tasks#new', as: :new
   get 'tasks/:id', to: 'tasks#show', as: :task
